fe152c4dd63bfec9bccefbe0c5305c99569d9eaf
yenle73 <128300242+yenle73@users.noreply.github.com>
2023-12-08 09:28:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import pickle as pkl
+from PIL import Image
 
+st.title('MNIST Prediction')
+
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
+
+image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+
+if image_file is not None:
+  image = load_image(image_file)
+  st.image(image)
